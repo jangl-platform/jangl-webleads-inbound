c69081e2a083c04bbf8bef658eb9d83dfaa42924
Ryan Anguiano <ryan.anguiano@gmail.com>
2019-06-05 01:00:58 -0400

    Add roof_shade to solar

--- jangl_webleads_inbound/verticals/solar.py
+++ jangl_webleads_inbound/verticals/solar.py
@@ -4,20 +4,22 @@ allow_blank = {'default': '', 'initial': '', 'allow_blank': True}
 
 
 class PingDataSerializer(serializers.Serializer):
-    best_call_time = serializers.CharField()
-    purchase_time_frame = serializers.CharField()
-    own_property = serializers.BooleanField()
+    best_call_time = serializers.CharField(**allow_blank)
+    purchase_time_frame = serializers.CharField(**allow_blank)
+    own_property = serializers.BooleanField(default=False)
     monthly_electric_bill = serializers.IntegerField()
-    utility_provider = serializers.CharField()
+    utility_provider = serializers.CharField(**allow_blank)
+    roof_shade = serializers.CharField(**allow_blank)
     property_type = serializers.CharField(max_length=30, **allow_blank)
     credit_rating = serializers.CharField(max_length=50, **allow_blank)
 
 
 class PostDataSerializer(serializers.Serializer):
-    best_call_time = serializers.CharField()
-    purchase_time_frame = serializers.CharField()
-    own_property = serializers.BooleanField()
+    best_call_time = serializers.CharField(**allow_blank)
+    purchase_time_frame = serializers.CharField(**allow_blank)
+    own_property = serializers.BooleanField(default=False)
     monthly_electric_bill = serializers.IntegerField()
-    utility_provider = serializers.CharField()
+    utility_provider = serializers.CharField(**allow_blank)
+    roof_shade = serializers.CharField(**allow_blank)
     property_type = serializers.CharField(max_length=30, **allow_blank)
     credit_rating = serializers.CharField(max_length=50, **allow_blank)
